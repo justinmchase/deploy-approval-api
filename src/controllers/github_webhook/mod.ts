e9a1113a26d4bcd580f16491b80fe8ef852a2daf
Justin Chase <justin.m.chase@gmail.com>
2023-08-06 17:08:48 -0500

    Start handling deployment protection rules

--- src/controllers/github_webhook/mod.ts
+++ src/controllers/github_webhook/mod.ts
@@ -1,24 +1,19 @@
-import { GithubWebhookController, ILogger, Response } from "grove/mod.ts";
+import {
+  github_api,
+  GithubWebhookController,
+  ILogger,
+  Response,
+} from "grove/mod.ts";
 import { Context, State } from "../../context.ts";
-import { GitHubInstallationEvent, GitHubPingEvent } from "github_api/mod.ts";
 
 export class DeployApprovalWebhookController
   extends GithubWebhookController<Context, State> {
-  protected override async handlePingEvent(
+  protected override async handleDeploymentProtectionRuleEvent(
     log: ILogger,
     res: Response,
-    event: GitHubPingEvent,
+    event: github_api.GitHubDeploymentProtectionRuleEvent,
   ): Promise<void> {
-    await log.debug("testing_ping", "its working...");
-    super.handlePingEvent(log, res, event);
-  }
-
-  protected override async handleInstallationEvent(
-    log: ILogger,
-    res: Response,
-    event: GitHubInstallationEvent,
-  ): Promise<void> {
-    await log.debug("testing_installation", "its working...");
-    super.handleInstallationEvent(log, res, event);
+    await log.debug("custom logic...", "goes here...");
+    await super.handleDeploymentProtectionRuleEvent(log, res, event);
   }
 }
