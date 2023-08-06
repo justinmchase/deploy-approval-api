e30f922355d2baf4b0187ea88bb5c64d7250ad81
Justin Chase <justin.m.chase@gmail.com>
2023-08-06 16:09:22 -0500

    override installation

--- src/controllers/github_webhook/mod.ts
+++ src/controllers/github_webhook/mod.ts
@@ -1,13 +1,24 @@
-import { GithubWebhookController } from "grove/mod.ts";
+import { GithubWebhookController, ILogger, Response } from "grove/mod.ts";
 import { Context, State } from "../../context.ts";
-import { GitHubPingEvent } from "https://deno.land/x/github_api@0.4.6/src/types/ping.ts";
-import { ILogger } from "https://deno.land/x/grove@0.5.0-pre.3/src/logging/logger.interface.ts";
-import { Response } from "https://deno.land/x/grove@0.5.0-pre.3/src/mod.ts";
+import { GitHubInstallationEvent, GitHubPingEvent } from "github_api/mod.ts";
 
 export class DeployApprovalWebhookController
   extends GithubWebhookController<Context, State> {
+  protected override async handlePingEvent(
+    log: ILogger,
+    res: Response,
+    event: GitHubPingEvent,
+  ): Promise<void> {
+    await log.debug("testing_ping", "its working...");
+    super.handlePingEvent(log, res, event);
+  }
 
-    protected override async handlePingEvent(log: ILogger, _res: Response, _event: GitHubPingEvent): Promise<void> {
-        await log.debug("testing", "its working...");
-    }
+  protected override async handleInstallationEvent(
+    log: ILogger,
+    res: Response,
+    event: GitHubInstallationEvent,
+  ): Promise<void> {
+    await log.debug("testing_installation", "its working...");
+    super.handleInstallationEvent(log, res, event);
+  }
 }
