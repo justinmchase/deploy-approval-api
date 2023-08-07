d0a8b07288c03324c592b91d69346e04d557b069
Justin Chase <justin.m.chase@gmail.com>
2023-08-06 20:24:28 -0500

    invoke the approval workflow

--- src/controllers/github_webhook/mod.ts
+++ src/controllers/github_webhook/mod.ts
@@ -1,3 +1,4 @@
+import { basename } from "std/path/mod.ts";
 import {
   github_api,
   GithubWebhookController,
@@ -13,7 +14,50 @@ export class DeployApprovalWebhookController
     res: Response,
     event: github_api.GitHubDeploymentProtectionRuleEvent,
   ): Promise<void> {
+    const {
+      deployment: { ref },
+      installation: { id: installationId },
+      repository,
+    } = event;
     await log.debug("custom logic...", "goes here...");
+    const client = await this.github.client(installationId);
+    const workflows = await github_api.api.repos.actions.workflows.list<
+      github_api.GitHubWorkflow
+    >({
+      client,
+      repository,
+    });
+    const approvalWorkflow = workflows.find((w) =>
+      basename(w.path) === "approval.yml"
+    );
+    if (!approvalWorkflow || approvalWorkflow.state !== "active") {
+      log.info(
+        "approval_workflow_not_available",
+        "approval workflow is not available",
+        {
+          approvalWorkflow,
+        },
+      );
+    } else {
+      const { id: workflowId } = approvalWorkflow;
+      await github_api.api.repos.actions.workflows.dispatches.create({
+        client,
+        ref,
+        repository,
+        workflowId,
+        inputs: {},
+      });
+      log.info(
+        "approval_workflow_dispatched",
+        `approval workflow ${workflowId} invoked for ${repository.full_name}`,
+        {
+          ref,
+          repository: { full_name: repository.full_name },
+          workflowId,
+        },
+      );
+    }
+
     await super.handleDeploymentProtectionRuleEvent(log, res, event);
   }
 }
